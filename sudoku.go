be30b4397255f0c19f470514842548d16e2c78d0
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2021-09-12 16:04:12 -0400

    Add some comments to explain how the algorithm works

--- sudoku.go
+++ sudoku.go
@@ -41,12 +41,16 @@ func (s *Sudoku) Fill() {
 		history = append(history, i)
 		occurances := 0
 
+		// Count the number of times the current box index has been processed in the past
+		// few loops.
 		for _, v := range history {
 			if v == i {
 				occurances++
 			}
 		}
 
+		// If the current board composition has been lead into an infinite loop, we want
+		// to empty the current board and start over.
 		if len(history) > 5 && occurances > len(history)/2 {
 			for _, box := range s.Board {
 				box.Empty()
@@ -58,12 +62,16 @@ func (s *Sudoku) Fill() {
 		}
 
 		box := s.Board[i]
+
+		// Empty a box before it's processed.
 		s.Board[i].Empty()
+
 		var boxH1 *Box
 		var boxH2 *Box
 		var boxV1 *Box
 		var boxV2 *Box
 
+		// Get the adjacent vertical boxes.
 		if i < 3 {
 			boxV1 = s.Board[i+3]
 			boxV2 = s.Board[i+6]
@@ -75,6 +83,7 @@ func (s *Sudoku) Fill() {
 			boxV2 = s.Board[i-6]
 		}
 
+		// Get the adjacent horizontal boxes.
 		if i%3 == 0 {
 			boxH1 = s.Board[i+1]
 			boxH2 = s.Board[i+2]
@@ -89,6 +98,7 @@ func (s *Sudoku) Fill() {
 		for j := 0; j < 9; j++ {
 			row := make([]uint8, 9)
 
+			// Get all the numbes in the current row.
 			if j < 3 {
 				copy(row, boxH1.GetRow(0))
 				row = append(row, boxH2.GetRow(0)...)
@@ -100,10 +110,13 @@ func (s *Sudoku) Fill() {
 				row = append(row, boxH2.GetRow(2)...)
 			}
 
+			// Get all the numbers in the current column.
 			col := append(boxV1.GetCol(j%3), boxV2.GetCol(j%3)...)
 
 			possible := make([]uint8, 0)
 
+			// Get the list of all of the numbers that can possibly apply to
+			// the current cell.
 			for k := uint8(1); k <= 9; k++ {
 				isPossible := true
 
@@ -130,9 +143,12 @@ func (s *Sudoku) Fill() {
 				}
 			}
 
+			// If the possible choices have been exahusted, we go to the previous box
+			// and set it up from scratch. There is a possibility that this back-tracking
+			// could lead to an infinite loop, but the circuit-breaker will prevent this
+			// by resetting the entire board and starting again, without resetting the
+			// seed that was used.
 			if len(possible) == 0 {
-				// bI = i
-
 				if i <= 2 {
 					i = 0
 				} else {
@@ -142,6 +158,8 @@ func (s *Sudoku) Fill() {
 				break
 			}
 
+			// Get a random number from all the possibilities and insert it in the target
+			// position of the current box.
 			num := possible[rand.Intn(len(possible))]
 			box.InsertPos(j, num)
 		}
